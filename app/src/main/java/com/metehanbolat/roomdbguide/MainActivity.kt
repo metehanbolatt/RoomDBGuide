d71ee7d31e7b1b9a49390f23b1006e0569a6c967
Metehan <metehan.bolat.ie@gmail.com>
2022-12-25 15:05:44 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/roomdbguide/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.metehanbolat.roomdbguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.metehanbolat.roomdbguide.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+    }
+}
